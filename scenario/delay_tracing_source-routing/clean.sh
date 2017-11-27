3d0d40ff4f1311a0153e778adee4cf13a92335f4
root <root@ezhecnx-0.seli.gic.ericsson.se>
2017-11-27 11:30:36 +0100

    add control script for the tracing secnario

---
+++ scenario/delay_tracing_source-routing/clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl delete -f delay_tracing_source-routing/deployment-svc-d-backup.yaml
+kubectl delete -f delay_tracing_source-routing/route-svc-d-backup-from-svc-f.yaml
+kubectl delete -f delay_tracing_source-routing/delay-svc-d-30ms.yaml
+kubectl delete -f delay_tracing_source-routing/delay-svc-c-20ms.yaml
+
---
+++ scenario/delay_tracing_source-routing/init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kubectl apply -f delay_tracing_source-routing/delay-svc-c-20ms.yaml
