759cd8410a3893830e98da381c3df27468a3e5b3
root <root@ezhecnx-0.seli.gic.ericsson.se>
2017-11-27 14:34:01 +0100

    tune the performance

--- scenario/destination_limit-rate/init.sh
+++ scenario/destination_limit-rate/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name loader-1 --rm istio/fortio load -c 2 -qps 80 -t 30m http://136.225.241.173:32000/api/svc-a/svc-b_svc-d/svc-c/
+docker run -d --name loader-1 --rm istio/fortio load -c 2 -qps 250 -t 30m http://136.225.241.173:32000/api/svc-a/svc-b_svc-d/svc-c/
