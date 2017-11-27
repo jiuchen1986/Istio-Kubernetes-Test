8ec69bee808c4d1f6be04faae2816f940da33c5c
root <root@ezhecnx-0.seli.gic.ericsson.se>
2017-11-27 11:49:25 +0100

    clean the loader when exit

--- scenario/delay_tracing_source-routing/clean.sh
+++ scenario/delay_tracing_source-routing/clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker stop loader-1
+docker stop loader-2
 kubectl delete -f delay_tracing_source-routing/deployment-svc-d-backup.yaml
 kubectl delete -f delay_tracing_source-routing/route-svc-d-backup-from-svc-f.yaml
 kubectl delete -f delay_tracing_source-routing/delay-svc-d-30ms.yaml
